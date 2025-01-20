2ec30c43797aa45dcc1cc82efef98c28c6856ee7
Jairo Júnior <jairojjr2003@gmail.com>
2025-01-20 01:39:51 -0300

    feat: made the authorization base, methods like getPatient, etc. now need an authorization otherwise u can't get them, but sign in, sign up, don't need it, couse don't make sense

--- clinic_project_back_end/handler.js
+++ clinic_project_back_end/handler.js
@@ -2,7 +2,7 @@ export async function hello(event) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Go Serverless v4! Your function executed successfully!",
+      message: "Go Serverless v4! Your function executed successfully! ",
     }),
   };
 }
